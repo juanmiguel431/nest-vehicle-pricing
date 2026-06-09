6c8f892cec8d23f195320b017c3179a7f3273a4b
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-09 13:58:15 -0400

    add GET /users/:id endpoint with validation and error handling in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  private service: UsersService;
 
+  constructor(service: UsersService) {
+    this.service = service;
+  }
+
+  @Get(':id')
+  async find(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+
+    const user = await this.service.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
 }
