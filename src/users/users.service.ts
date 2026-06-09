e038dc80bf3540a4cccea255b6a3ae51851391a7
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-08 22:52:10 -0400

    expand UsersService with CRUD operations for user management

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,7 +12,35 @@ export class UsersService {
   }
 
   create(email: string, password: string) {
-    const user = this.repository.create({email, password});
+    const user = this.repository.create({ email, password });
     return this.repository.save(user);
+  }
+
+  async findById(id: number) {
+    return this.repository.findOneBy({ id });
+  }
+
+  findByEmail(email: string) {
+    return this.repository.findOne({ where: { email } });
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    return this.repository.update(id, attrs);
+  }
+
+  async updatePassword(id: number, newPassword: string) {
+    return this.repository.update(id, { password: newPassword });
+  }
+
+  async remove(id: number) {
+    return this.repository.delete(id);
+  }
+
+  async removeAll() {
+    return this.repository.clear();
   }
 }
