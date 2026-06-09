961db61d581c3e40b4dbb74ab9a8a5ae564cfa7b
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-08 21:39:21 -0400

    integrate UsersService with TypeORM and update AuthController to handle user creation during signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
+  private userService: UsersService;
+
+  constructor(userService: UsersService) {
+    this.userService = userService;
+  }
 
   @Post('signup')
-  signup(@Body() body: CreateUserDto) {
+  async signup(@Body() body: CreateUserDto) {
 
+    const result = await this.userService.create(body.email, body.password);
     console.log(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private repository: Repository<User>;
+
+  constructor(@InjectRepository(User) repository: Repository<User>) {
+    this.repository = repository;
+  }
+
+  create(email: string, password: string) {
+    const user = this.repository.create({email, password});
+    return this.repository.save(user);
+  }
+}
