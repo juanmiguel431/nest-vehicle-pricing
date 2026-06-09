e1b2f6e3c77c88d4b1f5dadcae0db3370b8f17be
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-08 20:40:24 -0400

    add Report entity and integrate with TypeORM in ReportsModule and AppModule

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../users/user.entity';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+}
