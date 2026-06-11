87bcee03464dda3521d856a172a5cb11cb61d284
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-11 19:26:10 -0400

    update AuthService unit tests to include mock UsersService and add basic test case for service instance creation

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const user = new User();
+    user.id = 1;
+    user.email = 'test@test.com';
+    user.password = '123456';
+
+    const fakeUsersService = {
+      findById: (id: number) => Promise.resolve<User | null>(null),
+      findByEmail: (email: string) => Promise.resolve<User | null>(null),
+      find: () => Promise.resolve<User[]>([]),
+      create: (email: string, password: string) => Promise.resolve<User>(user),
+      remove: (entity: User) => Promise.resolve<User>(user),
+      update: (target: User, source: Partial<User>) =>
+        Promise.resolve<User>(user),
+    };
+
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +39,36 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+});
+
+
+it('can create an instance of AuthService', async () => {
+
+  const user = new User();
+  user.id = 1;
+  user.email = 'test@test.com';
+  user.password = '123456';
+
+  const fakeUsersService = {
+    findById: (id: number) => Promise.resolve<User | null>(null),
+    findByEmail: (email: string) => Promise.resolve<User | null>(null),
+    find: () => Promise.resolve<User[]>([]),
+    create: (email: string, password: string) => Promise.resolve<User>(user),
+    remove: (entity: User) => Promise.resolve<User>(user),
+    update: (target: User, source: Partial<User>) => Promise.resolve<User>(user),
+  };
+
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
 });
