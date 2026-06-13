045de82234d7456278b1c5b20baf22ade88e4138
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-13 15:10:57 -0400

    add create-report endpoint with DTO validation; update Report entity; implement service layer for report creation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  private reportsService: ReportsService;
+
+  constructor(reportService: ReportsService) {
+    this.reportsService = reportService;
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  create(@Body() body: CreateReportDto) {
+
+    return this.reportsService.create(body);
+
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  private repository: Repository<Report>;
+
+  constructor(@InjectRepository(Report) repository: Repository<Report>) {
+    this.repository = repository;
+  }
+
+  create(dto: CreateReportDto) {
+    const instance = this.repository.create(dto);
+    return this.repository.save(instance);
+  }
+}
