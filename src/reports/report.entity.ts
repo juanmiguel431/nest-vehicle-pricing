2dad1c8d9c039cd64bad200cdd984d218c4384c7
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-13 19:59:48 -0400

    establish one-to-many relationship between `User` and `Report` entities

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from '../users/user.entity';
 
 @Entity()
@@ -26,4 +26,7 @@ export class Report {
 
   @Column()
   millage: number;
+
+  @ManyToOne(() => User, user => user.reports)
+  user: User;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate, OneToMany } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -10,6 +11,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, report => report.user)
+  reports: Report[];
 
   @AfterInsert()
   private afterInsert() {
