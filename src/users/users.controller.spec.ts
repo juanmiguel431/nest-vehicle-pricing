993d5ddd9b4efe933abb153352df23d62a19c40c
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-12 23:58:42 -0400

    add mock UsersService implementation to UsersController unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { FindManyOptions } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findById: (id: number) => Promise.resolve<User | null>(null),
+      find: (options?: FindManyOptions<User> | undefined) => Promise.resolve<User[]>([]),
+      remove: (entity) => Promise.resolve<User>(entity),
+      update: (target: User, source: Partial<User>) => Promise.resolve<User>(target),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
