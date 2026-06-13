8fd54e19f57e91b97451c28f6876a891b56c94c2
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-13 00:25:56 -0400

    add unit test for AuthController to verify signIn returns user and updates session

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
+import { SignInDto } from '../users/dtos/sign-in.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -31,5 +33,25 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('signIn should return a user when provided correct credentials and updates session', async () => {
+    // fakeAuthService.signIn = jest.fn().mockResolvedValue({ id: 1, email: 'test@example.com' });
+    const user = new User();
+
+    fakeAuthService.signIn = (email, password) => {
+      user.id = 1;
+      user.email = email;
+      return Promise.resolve(user);
+    };
+
+    const session: Record<string, any> = {};
+    const body = new SignInDto();
+    body.email = 'test@example.com';
+    body.password = 'password';
+    const result = await controller.signIn(body, session);
+
+    expect(result.email).toEqual('test@example.com');
+    expect(session.userId).toEqual(1);
   });
 });
