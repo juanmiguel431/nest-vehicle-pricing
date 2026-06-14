098515564f52bb2cdf7026f2bc188ce4d01fac6b
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-13 23:41:14 -0400

    refactor `CurrentUserMiddleware`: simplify `Request` and `SessionData` type declarations and remove `AppRequest` usage

--- src/auth/middlewares/current-user.middleware.ts
+++ src/auth/middlewares/current-user.middleware.ts
@@ -3,23 +3,19 @@ import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../../users/users.service';
 import { User } from '../../users/user.entity';
 
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       currentUser?: User | null;
-//       session: {
-//         userId?: number;
-//       };
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User | null;
+    }
+  }
+}
 
-export type AppRequest = Request & {
-  currentUser?: User | null;
-  session: {
-    userId?: number;
-  };
-};
+declare module 'express-session' {
+  interface SessionData {
+      userId?: number;
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware<Request, Response> {
@@ -29,7 +25,7 @@ export class CurrentUserMiddleware implements NestMiddleware<Request, Response> 
     this.userService = userService;
   }
 
-  async use(req: AppRequest, res: Response, next: NextFunction) {
+  async use(req: Request, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
 
     if (userId) {
