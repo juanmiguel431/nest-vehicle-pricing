687393c762a89fa68588b0d285fac0f7d959802d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-13 23:33:22 -0400

    refactor `CurrentUserMiddleware`: update `use` method to use `AppRequest` type and remove unnecessary TypeScript ignores

--- src/auth/middlewares/current-user.middleware.ts
+++ src/auth/middlewares/current-user.middleware.ts
@@ -1,6 +1,25 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../../users/users.service';
+import { User } from '../../users/user.entity';
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       currentUser?: User | null;
+//       session: {
+//         userId?: number;
+//       };
+//     }
+//   }
+// }
+
+export type AppRequest = Request & {
+  currentUser?: User | null;
+  session: {
+    userId?: number;
+  };
+};
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware<Request, Response> {
@@ -10,12 +29,10 @@ export class CurrentUserMiddleware implements NestMiddleware<Request, Response> 
     this.userService = userService;
   }
 
-  async use(req: Request, res: Response, next: NextFunction) {
-    // @ts-ignore
+  async use(req: AppRequest, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
 
     if (userId) {
-      // @ts-ignore
       req.currentUser = await this.userService.findById(userId);
     }
 
